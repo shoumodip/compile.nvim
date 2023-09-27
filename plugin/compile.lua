0ed4f6546c9a95e9f1e9a17337f28ec845e0d23b
Shoumodip Kar <shoumodipkar@gmail.com>
2023-09-27 21:26:55 +0530

    Remove outdated commands

--- plugin/compile.lua
+++ plugin/compile.lua
@@ -2,7 +2,5 @@ vim.cmd([[
   command! -nargs=0 Recompile lua require("compile").restart(true)
   command! -nargs=0 CompileNext lua require("compile").next()
   command! -nargs=0 CompilePrev lua require("compile").prev()
-  command! -nargs=0 CompileOpen lua require("compile").file()
-  command! -nargs=0 CompileStop lua require("compile").interrupt()
   command! -nargs=* -complete=file Compile lua require("compile").start(<q-args>)
 ]])
